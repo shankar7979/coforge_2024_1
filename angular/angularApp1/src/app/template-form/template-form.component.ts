c249f5c571c7b5467004cc1073224fd1382d0dd7
shankar7979 <shankar7979@gmail.com>
2024-06-27 16:42:10 +0530

    added

--- angular/angularApp1/src/app/template-form/template-form.component.ts
+++ angular/angularApp1/src/app/template-form/template-form.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Customer } from './model/Customer';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './template-form.component.html',
   styleUrl: './template-form.component.css'
 })
 export class TemplateFormComponent {
 
-  customer:Customer;
+  @Input() customer:Customer;
+
+  visible:boolean;
 
   constructor(){
+    this.visible=false;
     this.customer=new Customer();
+  }
+
+  submit(){
+     this.visible=true;
   }
 
 }
