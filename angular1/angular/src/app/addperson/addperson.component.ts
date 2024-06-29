2fa38df8621a948ab0fd34f0a7cd6a2af07318a2
shankar7979 <shankar7979@gmail.com>
2024-06-29 11:19:18 +0530

    added

--- angular1/angular/src/app/addperson/addperson.component.ts
+++ angular1/angular/src/app/addperson/addperson.component.ts
@@ -2,18 +2,20 @@ import { Component, Input } from '@angular/core';
 import { PersonService } from '../person-app/service/person.service';
 import { Observable } from 'rxjs';
 import { Person } from '../person-app/model/person';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-addperson',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './addperson.component.html',
   styleUrl: './addperson.component.css'
 })
 export class AddpersonComponent {
 
-  msg: string = '';
+  msg: string = 'welcome';
   @Input() person: Person;
 
   constructor(private service: PersonService) {
@@ -22,5 +24,6 @@ export class AddpersonComponent {
 
   savePerson() {
     this.service.addPerson(this.person).subscribe(data => this.msg = "Record Added", error => this.msg = error.error);
+    console.log(this.msg)
   }
 }
