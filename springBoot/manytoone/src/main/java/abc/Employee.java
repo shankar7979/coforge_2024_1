65900d681a240bc0ab47861e2a2a8c0be166be4a
shankar7979 <shankar7979@gmail.com>
2024-07-04 12:51:41 +0530

    added

---
+++ springBoot/manytoone/src/main/java/abc/Employee.java
@@ -0,0 +1,20 @@
+package abc;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @ManyToOne//(cascade = CascadeType.ALL)
+    private Address address;
+
+
+}
