f8f18a3867c1688c9349fe41cbeb1a7228ede36e
shankar7979 <shankar7979@gmail.com>
2024-07-15 16:42:19 +0530

    added

--- angular security jwt/security app/angular1/src/app/app.component.ts
+++ angular security jwt/security app/angular1/src/app/app.component.ts
@@ -1,18 +1,15 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecurityComponent } from './security/security.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SecurityComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Demo';
-  greeting:any = {};
-  constructor(private http: HttpClient) {
-    http.get('resource').subscribe(data => this.greeting = data);
-  }
+  title:string='angular security'
 }
--- angular security jwt/security app/angular1/src/app/app.routes.ts
+++ angular security jwt/security app/angular1/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'home'},
-  { path: 'home', component: HomeComponent},
-  { path: 'login', component: LoginComponent}
+    // {path:"",redirectTo:"login",pathMatch:"full"},
+    // {path:"login",component:LoginComponent},
+    // {path:"home",component:HomeComponent},
 ];
